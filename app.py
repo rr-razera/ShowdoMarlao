647f2e9651440441fbfa78584aa0e9d162a67ea0
rr-razera <35960960+rr-razera@users.noreply.github.com>
2025-09-25 11:09:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -15,37 +15,81 @@ def avancar_fase():
 # Perguntas
 perguntas = {
     1: {
-        "titulo": "FASE 1: CONHECIMENTO GERAL",
-        "pergunta": "Qual é a principal forma de proteção da Ilha do Campeche?",
+        "titulo": "MÀ OE! Bem vindo ao Show Da Governança",
+        "pergunta": "Vamos a primeira pergunta:O que é a governança pública?",
         "opcoes": [
-            "A) É um Parque Nacional.",
-            "B) É uma Área de Proteção Ambiental (APA).",
-            "C) É uma Reserva Biológica.",
-            "D) É um Sítio do Patrimônio Mundial da UNESCO."
+            "A) Conjunto de instituições que atuam de forma independente na gestão de um bem público",
+            "B) Sistema de regras, princípios e mecanismos de gestão e controle para tomar decisões de forma ética, responsável e transparente, visando alcançar objetivos coletivos e atender ao interesse público",
+            "C) Sistema de regras que visa interesses particulares por meio da exploração de um bem público",
+            "D) Forma de governo em que apenas líderes políticos tomam todas as decisões sem participação social"
         ],
-        "correta": "B) É uma Área de Proteção Ambiental (APA)."
+        "correta": "B) Sistema de regras, princípios e mecanismos de gestão e controle para tomar decisões de forma ética, responsável e transparente, visando alcançar objetivos coletivos e atender ao interesse público"
     },
     2: {
-        "titulo": "FASE 2: REGRAS DE VISITAÇÃO",
-        "pergunta": "Para manter a preservação, qual das seguintes regras de visitação é VERDADEIRA?",
+        "titulo": "Muito bem! Vamos a proxima pergunta",
+        "pergunta": "Quais áreas a governança pública eficaz deve abranger?",
         "opcoes": [
-            "A) É permitido acampar com autorização prévia.",
-            "B) É proibido o uso de protetor solar biodegradável.",
-            "C) É permitido levar animais de estimação, desde que fiquem na coleira.",
-            "D) O número de visitantes por dia é limitado para minimizar o impacto ambiental."
+            "A) Social, Econômica e Ambiental",
+            "B) Iniciativa privada, Lucratividade",
+            "C) Orçamento participativo, Hierarquia de cargos",
+            "D) Esportiva, Religiosa e Militar"
         ],
-        "correta": "D) O número de visitantes por dia é limitado para minimizar o impacto ambiental."
+        "correta": "A) Social, Econômica e Ambiental"
     },
     3: {
-        "titulo": "FASE 3: BIODIVERSIDADE",
-        "pergunta": "Qual das seguintes espécies é Nativa da Ilha do Campeche?",
+        "titulo": "Má oe, ma vem pra cá",
+        "pergunta": "Proxima pergunta: A governança pública deve garantir:",
         "opcoes": [
-            "A) Arara Azul.",
-            "B) Tatu-bola.",
-            "C) Mico-leão-dourado.",
-            "D) Onça-pintada."
+            "A) Segurança, Benefícios individuais, Conservação",
+            "B) Investimentos, Desvio de recursos, Integridade pública",
+            "C) Transparência, prestação de contas, equidade e responsabilidade",
+            "D) Sigilo total das informações públicas"
         ],
-        "correta": "B) Tatu-bola."
+        "correta": "C) Transparência, prestação de contas, equidade e responsabilidade"
+    },
+     4: {
+        "titulo": "vem pra cá vem pra cá",
+        "pergunta": "Proxima pergunta: A Ilha do Campeche foi tombada em qual ano?",
+        "opcoes": [
+            "A) 1974",
+            "B) 1995",
+            "C) 2000",
+            "D) 1988"
+        ],
+        "correta": "C) 2000"
+    },
+      5: {
+        "titulo": "É com você, Lombardi!",
+        "pergunta": "Proxima pergunta: Segundo a legislação vigente o número de visitantes permitido por dia na Ilha do Campeche é de:",
+        "opcoes": [
+            "A) 2000 visitantes por dia",
+            "B) 1200 visitantes por dia",
+            "C) 800 visitantes por dia",
+            "D) 500 visitantes por dia"
+        ],
+        "correta": "C) 800 visitantes por dia"
+    },
+    6: {
+        "titulo": "Olha a Tele-Sena",
+        "pergunta": "Proxima pergunta: Um dos principais desafios da governança na Ilha do Campeche é:",
+        "opcoes": [
+            "A) Manter o acesso, simplificando a fiscalização de transporte e ingressos",
+            "B) Aumentar o número de visitantes para gerar mais renda para a comunidade local",
+            "C) Equilibrar a conservação ambiental com o turismo sustentável",
+            "D) Garantir a autoridade da comunidade local nas decisões"
+        ],
+        "correta": "C) Equilibrar a conservação ambiental com o turismo sustentável"
+    },
+    7: {
+        "titulo": "Ultima pergunta, valendo um milhão de gerais",
+        "pergunta": "A participação da comunidade na governança da Ilha do Campeche é importante porque...",
+        "opcoes": [
+            "A) Facilita a exploração sem regulamentação por parte de empresas privadas",
+            "B) É irrelevante, pois somente órgãos federais decidem sobre o patrimônio",
+            "C) Serve apenas para fins decorativos em reuniões públicas",
+            "D) Garante que os interesses locais e o conhecimento tradicional sejam considerados na gestão"
+        ],
+        "correta": "D) Garante que os interesses locais e o conhecimento tradicional sejam considerados na gestão"
     }
 }
 
